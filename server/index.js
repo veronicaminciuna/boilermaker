e2c3418c05bd6fbbc4b96765e3da6f2939ada443
Veronica Minciuna <veronicaminci@gmail.com>
2020-06-12 16:24:54 -0400

    top level files

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const passport = require("passport");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const compression = require("compression");
+const SequelizeStore = require("connect-session-sequelize");
+const db = require("./db");
+const sessionStore = new SequelizeStore({ db });
+const PORT = process.env.PORT || 8080;
+const app = express();
+const socketio = require("socket.io");
+
+module.exports = app;
