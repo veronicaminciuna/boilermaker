0791b4cba23f0c47f294aa011c1a019a35d27215
Veronica Minciuna <veronicaminci@gmail.com>
2020-06-13 00:14:00 -0400

    server

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 module.exports = router;
 
+router.use("/users", require("./users"));
+
 router.use((req, res, next) => {
   const error = new Error("Not Found");
   error.status = 404;
